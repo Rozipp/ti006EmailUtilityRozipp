25b1f1edbfa925acad753dd07d1bb3b5c19cad2e
Rozipp <Royashch@gmail.com>
2023-03-20 17:46:50 +0200

    Next2

---
+++ src/main/java/ua/rozipp/EmailUtility.java
@@ -0,0 +1,12 @@
+package ua.rozipp;
+
+public class EmailUtility {
+
+    public static boolean checkEmail(String text) {
+        if (text == null) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/ua/rozipp/Main.java
@@ -0,0 +1,8 @@
+package ua.rozipp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Begin");
+    }
+}
